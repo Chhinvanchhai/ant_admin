da727d1445cbb51199ac5ba36fa52113631cf8a9
Chhinvanchhai <chhai.chhin51@gmail.com>
2023-08-17 21:36:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Table from './views/Table.vue'
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
